1a8d663d8990e473900c2dd12c3b6572948bc4cb
Edwin Dertien <e.dertien@utwente.nl>
2025-10-29 17:30:15 +0100

    usbasp firmware (working) for clones

--- usbasp/firmware/usbconfig.h
+++ usbasp/firmware/usbconfig.h
@@ -21,13 +21,6 @@ the newest features and options.
 */
 
 /* ---------------------------- Hardware Config ---------------------------- */
-// for ATmega32 on USBprog
-// #define USB_CFG_IOPORTNAME      D
-// #define USB_CFG_DMINUS_BIT      4
-// #define USB_CFG_DPLUS_BIT       2
-// #define USB_CFG_CLOCK_KHZ       12000
-// #define USB_CFG_PULLUP_IOPORTNAME D
-// #define USB_CFG_PULLUP_BIT        3
 
 // For USBASP (clone) on Atmega88
 
